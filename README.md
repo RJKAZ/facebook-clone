1a88b60385afd3c5d7a355d460a2bd4d15c7de32
Richard Kazanowski <rjkaz@comcast.net>
2020-11-14 15:38:44 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # facebook-clone
 Following a youtube tutorial to create a clone of facebook using React.
+
+Left off on the video at the 1:22:22 mark
--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import './App.css';
-import Header from "./Header"
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     // BEM naming convention 
     <div className="app">
-      
+
 
       <Header />
+      <div className="app__body">
+        <Sidebar />
 
-      {/* Sidebar */}
+        {/* Sidebar */}
         {/* Feed */}
         {/* Widgets */}
+      </div>
     </div>
 
   );
